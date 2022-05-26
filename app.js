be4078606e08236f6c174feb71cb15036f3ccee1
Rexie Esmeralda <esmeraldarexie@gmail.com>
2022-05-25 21:16:36 -0700

    added js

---
+++ app.js
@@ -0,0 +1,7 @@
+let askMessage = prompt("Do you like fruits?");
+if (askMessage == "yes")
+    document.write("Hello, fellow fruit person!");
+    else if (askMessage == "no")
+    document.write("Hello, non-fruit person!");
+    else if (askMessage =="")
+    document.write("So you do not have a name?")
